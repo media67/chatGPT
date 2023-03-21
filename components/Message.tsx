3b94d426bc4020357a504f4cc58f42227a395a53
Samuel Bourassa <district67@gmail.com>
2023-03-21 13:09:53 -0400

    Finish ChatInput component and askQuestion enpoint.

---
+++ components/Message.tsx
@@ -0,0 +1,13 @@
+import { DocumentData } from "firebase/firestore"
+
+type Props = {
+    message: DocumentData
+}
+
+function Message({ message }: Props) {
+  return (
+    <div>Message</div>
+  )
+}
+
+export default Message
